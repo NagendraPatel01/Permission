659924b96beee1dd1148516a51fa9cd6b8a10d61
Bhupendra Patel <bhupendra.bp57@gmail.com>
2021-08-13 23:19:04 +0530

    permission is completed

--- app/src/main/java/com/hackerkernel/myapplication/MainActivity.java
+++ app/src/main/java/com/hackerkernel/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.hackerkernel.myapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -12,9 +13,10 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int SELECT_IMAGE =1 ;
     Button btn;
 
-    String[] permission={"android.permission.CALL_PHONE","android.permission.WRITE_EXTERNAL_STORAGE"};
+    String[] permission={"android.permission.WRITE_EXTERNAL_STORAGE"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +47,12 @@ public class MainActivity extends AppCompatActivity {
 
             if (grantResults[0]== PackageManager.PERMISSION_GRANTED){
 
-                Toast.makeText(this, "Download code", Toast.LENGTH_SHORT).show();
+               // Toast.makeText(this, "Download code", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"),SELECT_IMAGE);
             }
 
             else {
